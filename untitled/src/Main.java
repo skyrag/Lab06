b13c32e0b05b49005eba26fda3e0319557f201fc
skyrag <duprat.nicolas@outlook.com>
2024-11-11 17:18:11 +0100

    adding src

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import calculator.JCalculator;
+
+public class Main
+{
+  public static void main(String ... args) {
+    new JCalculator();
+  }
 }
