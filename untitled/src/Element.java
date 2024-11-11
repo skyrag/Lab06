9334cfca8e9374aef246f397740accceb3afdc43
skyrag <duprat.nicolas@outlook.com>
2024-11-11 17:13:51 +0100

    delete simple list and elem own class

---
+++ untitled/src/Element.java
@@ -0,0 +1,21 @@
+public class Element<T> {
+    private Element<T> next;
+    private T value;
+
+    public Element(T value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Element<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Element<T> next) {
+        this.next = next;
+    }
+}
